e2372790562d4ec9e4f9d8a201507c6c3c12f5b3
IvanTheTerrible <plagues.wtp@gmail.com>
2024-04-04 14:19:15 +0300

    Change DB and ErrSettings

---
+++ controllers/product.js
@@ -0,0 +1,12 @@
+const getAllProductsStatic = async (req, res) => {
+    throw new Error('testing async pac')
+    res.status(200).json({ msg: 'product testing route' })
+}
+const getAllProducts = async (req, res) => {
+    res.status(200).json({ msg: 'product route' })
+}
+
+module.exports = {
+    getAllProducts,
+    getAllProductsStatic
+}
