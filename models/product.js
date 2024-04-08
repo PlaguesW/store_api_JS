df6468d5486c905e597090ca147d01185a056e2e
IvanTheTerrible <plagues.wtp@gmail.com>
2024-04-08 16:23:38 +0300

    Product changes

--- models/product.js
+++ models/product.js
@@ -1,34 +1,34 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required:[true,'product name must be provided']
+  name: {
+    type: String,
+    required: [true, "product name must be provided"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price must be provided"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "lamoda", "adidas", "nike"],
+      message: "{Value} is not supported",
     },
-    price:{
-        type: Number,
-        required:[true,'product price must be provided']
-    },
-    featured: {
-        type: Boolean,
-        default: false
-    },
-    rating:{
-        type: Number,
-        default: 4.5,
-    },
-    createAt:{
-        type: Date,
-        default: Date.now()
-    },
-    company:{
-        type: String,
-        enum:{
-            values: ['ikea', 'lamoda', 'adidas', 'nike'],
-            message: '{Value} is not supported',
-        }
-        //['ikea', 'lamoda', 'adidas', 'nike'],
-    },
-})
+    //['ikea', 'lamoda', 'adidas', 'nike'],
+  },
+});
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model("Product", productSchema);
